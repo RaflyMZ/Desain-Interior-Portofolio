7d2f43cddad54f68f3c719781cfb4ac82e416413
Rafly Maulana Zulyzar <rafly2027@gmail.com>
2026-09-20 09:52:33 +0700

    fix AppServiceProvide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Pastikan log diarahkan ke folder /tmp jika berjalan di Vercel
+        if (env('LOG_CHANNEL') === 'stderr' || isset($_ENV['NOW_REGION'])) {
+            $this->app->useStoragePath('/tmp/storage');
+        }
+
     }
 
     /**
--- config/logging.php
+++ config/logging.php
@@ -96,13 +96,11 @@ return [
 
         'stderr' => [
             'driver' => 'monolog',
-            'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => StreamHandler::class,
+            'handler' => Monolog\Handler\StreamHandler::class,
             'formatter' => env('LOG_STDERR_FORMATTER'),
             'with' => [
                 'stream' => 'php://stderr',
             ],
-            'processors' => [PsrLogMessageProcessor::class],
         ],
 
         'syslog' => [
